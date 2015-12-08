9f215b916f6fa1192e159e05ae1a6f50a927c87c
matseriknord <mats.nordh@gmail.com>
2015-12-08 20:44:55 +0100

    First version

--- FindWebLinks/src/FindingWebLinks/FindingWebLinks.java
+++ FindWebLinks/src/FindingWebLinks/FindingWebLinks.java
@@ -50,6 +50,18 @@ public class FindingWebLinks {
         }
         return store;
     }
+    public void findURLs2() {
+        URLResource file = new edu.duke.URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        for ( String item : file.words() ) {
+            String itemLower = item.toLowerCase();
+            int pos = itemLower.indexOf("youtube.com");
+            if ( pos != -1 ) {
+               int beg = item.lastIndexOf("\"", pos);
+               int end = item.indexOf("\"", pos + 1);
+               System.out.println(item.substring(beg + 1, end));
+            }
+        }
+    }
     public static void main(String[] args) {
         FindingWebLinks wl = new FindingWebLinks();
         StorageResource store = wl.findURLs("www.youtube.com");
